08a8ae14d372cabaf4b1cb55634bcb07f983239f
adetunmbi temiloluwa <adetunmbitemiloluwa.com>
2025-06-20 12:44:38 +0100

    added number

--- KG.py
+++ KG.py
@@ -7,3 +7,8 @@ def pounds_to_kilogram(pounds):
     kilogram = (pounds / 100) * 3.28
     print(kilogram)
 pounds_to_kilogram(40)
+
+number (12, 45, 67, 98, 90)
+number.append(50)
+number.append(70)
+number.append(90)
