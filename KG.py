1c48afc12fb39eefe5f3942babce1dc699566843
adetunmbi temiloluwa <adetunmbitemiloluwa.com>
2025-06-20 12:33:59 +0100

    first commit

---
+++ KG.py
@@ -0,0 +1,9 @@
+def kilogram_to_pounds(kilogram):
+    pounds = (kilogram / 100) * 3.28
+    print(pounds)
+kilogram_to_pounds(30)
+
+def pounds_to_kilogram(pounds):
+    kilogram = (pounds / 100) * 3.28
+    print(kilogram)
+pounds_to_kilogram(40)
---
+++ task 5.py
@@ -0,0 +1,13 @@
+from os import device_encoding
+
+numbers = [3, 12, 7, 14, 9, 20, 1]
+for number in numbers:
+    print(number)
+
+numbers = [3, 12, 7, 14, 9, 20, 1]
+for number in numbers:
+    if (number / 2) == 0:
+        print('the number is even')
+    else:
+        print('the number is odd')
+
